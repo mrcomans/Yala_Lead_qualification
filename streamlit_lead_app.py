e70390e7947f953a9e73ba72f2cc68fcbf926f1e
mrcomans <mark.teeken@gmail.com>
2023-12-11 13:46:05 +0100

    Update streamlit_lead_app.py

--- streamlit_lead_app.py
+++ streamlit_lead_app.py
@@ -1,1 +1,7 @@
-print(Hello)
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('Hello there let's score some leads')
