addf9be7b1a721c19e06b062a4363ef7ba649707
mrcomans <mark.teeken@gmail.com>
2023-12-12 21:52:36 +0100

    refresh

--- streamlit_lead_app.py
+++ streamlit_lead_app.py
@@ -42,7 +42,7 @@ def load_model_from_registry():
     # Create a registry object
     registry = model_registry.ModelRegistry(session=session, database_name=db, schema_name=schema, create_if_not_exists=True)
 
-    # Load the model
+    # Load the model v2
     st.write("models:", registry.list_models().to_pandas())
 
     # registry.list_models().to_pandas()
