d28540126c69f25987e108356c6afe88becdd831
mrcomans <mark.teeken@gmail.com>
2023-12-12 07:47:46 +0100

    Show records

--- streamlit_lead_app.py
+++ streamlit_lead_app.py
@@ -10,6 +10,8 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * FROM YALA_DB.PUBLIC.CONVERTEDONLY LIMIT 10")
 my_data_rows = my_cur.fetchall()
+streamlit.dataframe(my_data_rows)
 
 streamlit.title('Connected!')
