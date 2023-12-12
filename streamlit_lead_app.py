4d1a0440a3003645e1fc3a17ff4057bbd6b0f454
mrcomans <mark.teeken@gmail.com>
2023-12-12 07:36:33 +0100

    Added connection to app

--- streamlit_lead_app.py
+++ streamlit_lead_app.py
@@ -5,3 +5,8 @@ import snowflake.connector
 from urllib.error import URLError
 
 streamlit.title('Hello there let us score some leads')
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+
+streamlit.title('Connected!')
