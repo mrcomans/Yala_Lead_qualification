bc424f3aa098546c6fbe8340c9aaead4197e52b2
mrcomans <mark.teeken@gmail.com>
2023-12-12 07:39:27 +0100

    Execute SQL

--- streamlit_lead_app.py
+++ streamlit_lead_app.py
@@ -9,4 +9,6 @@ streamlit.title('Hello there let us score some leads')
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
 streamlit.title('Connected!')
