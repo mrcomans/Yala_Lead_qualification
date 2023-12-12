aa4e87ce5a4f3ee8be0b1fa3fcef4f188a83a6ec
mrcomans <mark.teeken@gmail.com>
2023-12-12 07:41:22 +0100

    fetchall

--- streamlit_lead_app.py
+++ streamlit_lead_app.py
@@ -10,5 +10,6 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_rows = my_cur.fetchall()
 
 streamlit.title('Connected!')
